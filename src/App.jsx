a54244a679c87a9653e08ec00e96cc27376c028f
nguyew <nguyenbaoquan159@gmail.com>
2026-09-25 10:20:07 +0700

    Create a React form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
