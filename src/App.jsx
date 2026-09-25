a43b55ab964b7b515a446492d20d01f22e6d3426
nguyew <nguyenbaoquan159@gmail.com>
2026-09-25 10:51:51 +0700

    Add Dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
